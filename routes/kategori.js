f2149ea6d7f9e68c0f424ff8849195842a64fa46
AldyGege <youngfather86@gmail.com>
2024-02-22 12:50:38 +0700

    Latihan5

--- routes/kategori.js
+++ routes/kategori.js
@@ -15,4 +15,66 @@ router.get('/', function(req, res) {
     });
 });
 
+router.get('/create', function(req, res, next) {
+    res.render('kategori/create', {
+    nama_kategori: ''
+    })
+});
+
+router.post('/store', function(req, res, next){
+    try {
+    let {nama_kategori} = req.body;
+    let Data = {
+        nama_kategori
+    }
+    connection.query('insert into kategori set ?', Data, function(err, result) {
+        if (err) {
+            req.flash('error', 'Gagal menyimpan data!');
+        }else{
+            req.flash('success', 'Berhasil menyimpan data!');
+        }
+            res.redirect('/kategori');
+    })
+    } catch {
+        req.flash('error', 'Terjadi kesalahan pada fungsi')
+        res.redirect('/kategori');
+    }
+});
+
+router.get('/edit/:id', function(req, res, next) {
+    let id = req.params.id;
+
+    connection.query('SELECT * FROM kategori WHERE id_kategori = ?', id, function(err, rows) {
+        if (err) {
+            req.flash('error', 'Query gagal!');
+        } else {
+            res.render('kategori/edit', {
+                id: rows[0].id_kategori,
+                nama_kategori: rows[0].nama_kategori
+            });
+        }
+    });
+});
+
+router.post('/update/:id', function(req, res, next) {
+    try {
+        let id = req.params.id;
+        let nama_kategori = req.body.nama_kategori;
+        let data = { nama_kategori };
+
+        connection.query('UPDATE kategori SET ? WHERE id_kategori = ' + id, data, function(err) {
+            if (err) {
+                req.flash('error', 'Gagal memperbarui data');
+            } else {
+                req.flash('success', 'Berhasil memperbarui data');
+                res.redirect('/kategori');
+            }
+        });
+    } catch (err) {
+        req.flash('error', 'Terjadi kesalahan pada fungsi');
+        res.render('/kategori');
+    }
+});
+
+
 module.exports = router;
